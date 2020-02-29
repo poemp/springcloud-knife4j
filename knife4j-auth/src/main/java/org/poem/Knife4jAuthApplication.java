db1bd33678b078f370f2b980aedc533cc0321c5e
lywwrking <xue_2013@sina.com>
2020-02-29 12:30:46 +0800

    修改nacos配置

--- knife4j-auth/src/main/java/org/poem/Knife4jAuthApplication.java
+++ knife4j-auth/src/main/java/org/poem/Knife4jAuthApplication.java
@@ -2,11 +2,13 @@ package org.poem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author poem
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Knife4jAuthApplication {
     
     public static void main(String[] args) {
--- knife4j-base/src/main/java/org/poem/Knife4jBaseApplication.java
+++ knife4j-base/src/main/java/org/poem/Knife4jBaseApplication.java
@@ -2,10 +2,12 @@ package org.poem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author poem
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Knife4jBaseApplication {
     
